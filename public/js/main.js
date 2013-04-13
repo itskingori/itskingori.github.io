31563ec1a0c00abf88d0abebe800ad93be655fca
King'ori Maina <j@kingori.co>
2013-04-14 00:53:55 +0300

    New logo & new article on it

--- public/js/main.js
+++ public/js/main.js
@@ -51,15 +51,16 @@ SyntaxHighlighter.all();
 var Chameleon = {};
 
 Chameleon.noOfColors = 10; // should be in CSS, with the transitions
-Chameleon.duration = 4; // should match the transition duration in css
+Chameleon.duration = 5; // should match the transition duration in css
 
 Chameleon.init = function() {
 
   // Check if we support CSS transitions on the browser
   if ( Modernizr.csstransitions ) {
 
-    // Grab the body, we will be using it a lot
+    // Grab the elements, we will be using it a lot
     Chameleon.bodyElement = $("body");
+    Chameleon.logoElement = $("header .logo");
 
     // ~~ faster than Math.floor() -> http://rocha.la/JavaScript-bitwise-operators-in-practice
     Chameleon.colorT = ~~(Math.random()*Chameleon.noOfColors);
@@ -71,8 +72,10 @@ Chameleon.init = function() {
 // Switch colors
 Chameleon.changeColor = function() {
   Chameleon.bodyElement.removeClass( 'color' + Chameleon.colorT % Chameleon.noOfColors );
+  Chameleon.logoElement.removeClass( 'bkgcolor' + Chameleon.colorT % Chameleon.noOfColors );
   Chameleon.colorT++;
   Chameleon.bodyElement.addClass( 'color' + Chameleon.colorT % Chameleon.noOfColors );
+  Chameleon.logoElement.addClass( 'bkgcolor' + Chameleon.colorT % Chameleon.noOfColors );
   setTimeout( Chameleon.changeColor, Chameleon.duration * 1000 );
 };
 
