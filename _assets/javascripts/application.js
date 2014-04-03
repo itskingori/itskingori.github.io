5b5b51eff24c18e439ca3f6c3fd5e9c1c95cfbb6
King'ori Maina <j@kingori.co>
2014-04-03 20:20:21 +0200

    Preprocess main.js
    * Minifying Turbolinks results in errors so we will just leave that one
    out.

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -4,4 +4,4 @@
 //= require ./vendor/highlight.pack
 //= require ./vendor/foundation
 //= require ./vendor/nprogress
-// require ./main
+//= require ./main
---
+++ _assets/javascripts/main.js
@@ -0,0 +1,49 @@
+// +++ SHIFTING COLORS ~ Chameleon
+var Chameleon = {};
+Chameleon.noOfColors = 10; // should be in CSS, with the transitions
+Chameleon.duration = 5;    // should match the transition duration in css
+
+Chameleon.init = function() {
+
+  // Check if we support CSS transitions on the browser
+  if ( Modernizr.csstransitions ) {
+
+    // Grab the elements, we will be using it a lot
+    Chameleon.bodyElement = $('body');
+    Chameleon.logoElement = $('header a#logo');
+
+    // ~~ faster than Math.floor() -> http://rocha.la/JavaScript-bitwise-operators-in-practice
+    Chameleon.colorT = ~~(Math.random()*Chameleon.noOfColors);
+    Chameleon.changeColor();
+  }
+}
+
+Chameleon.changeColor = function() {
+  Chameleon.bodyElement.removeClass( 'color' + Chameleon.colorT % Chameleon.noOfColors );
+  Chameleon.logoElement.removeClass( 'bkgcolor' + Chameleon.colorT % Chameleon.noOfColors );
+  Chameleon.colorT++;
+  Chameleon.bodyElement.addClass( 'color' + Chameleon.colorT % Chameleon.noOfColors );
+  Chameleon.logoElement.addClass( 'bkgcolor' + Chameleon.colorT % Chameleon.noOfColors );
+  setTimeout( Chameleon.changeColor, Chameleon.duration * 1000 );
+};
+
+// +++ SYNTAX HIGHLIGHTER
+var SyntaxHighlighter = {};
+SyntaxHighlighter.init = function() {
+  $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
+}
+
+// +++ NProgress
+$(document).on('page:fetch',   function() {
+  NProgress.start();
+});
+$(document).on('page:change',  function() {
+  NProgress.done();
+  Chameleon.init();
+  SyntaxHighlighter.init();
+});
+$(document).on('page:restore', function() {
+  NProgress.remove();
+  Chameleon.init();
+  SyntaxHighlighter.init();
+});
