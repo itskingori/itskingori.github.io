de8babf0070c6c1cf9addfcfbf2fa30b6920be43
King'ori Maina <j@kingori.co>
2014-04-03 15:09:34 +0200

    Preprocess NProgress and Foundation

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -2,6 +2,6 @@
 //= require ./vendor/jquery
 //= require ./vendor/less
 //= require ./vendor/highlight.pack
-// require ./vendor/foundation
-// require ./nprogress
+//= require ./vendor/foundation
+//= require ./vendor/nprogress
 // require ./main
--- _assets/javascripts/vendor/nprogress.js
+++ _assets/javascripts/vendor/nprogress.js
@@ -1,4 +1,4 @@
-/*! NProgress (c) 2013, Rico Sta. Cruz
+/* NProgress (c) 2013, Rico Sta. Cruz
  *  http://ricostacruz.com/nprogress */
 
 ;(function(factory) {
