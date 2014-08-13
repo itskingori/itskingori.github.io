4e4db88cfc99345b9cab7d5d78108fa03c6e22e0
King'ori Maina <j@kingori.co>
2014-08-13 21:41:21 +0200

    Use SASS instead of LESS

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -1,6 +1,5 @@
 //= require ./vendor/modernizr
 //= require ./vendor/jquery
-//= require ./vendor/less
 //= require ./vendor/highlight.pack
 //= require ./vendor/foundation
 //= require ./vendor/nprogress
